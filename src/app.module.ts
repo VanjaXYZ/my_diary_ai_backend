9144b986ab86ed2d6775c4039652e244153a3cf1
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-09 00:27:09 +0100

    add providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
+import { UserModule } from './user/user.module';
+import { DiaryEntriesModule } from './diary-entries/diary-entries.module';
+import { UserService } from './user/user.service';
+import { PrismaService } from './prisma.service';
+import { DiaryEntriesService } from './diary-entries/diary-entries.service';
+import { RegistrationModule } from './registration/registration.module';
 
 @Module({
-  imports: [LoginModule],
+  imports: [LoginModule, UserModule, DiaryEntriesModule, RegistrationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService, DiaryEntriesService],
 })
 export class AppModule {}
