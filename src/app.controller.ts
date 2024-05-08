fc88b29dc73a617dc1c440b1c6e4c589020837c4
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-09 00:27:03 +0100

    created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,68 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { DiaryEntriesService } from './diary-entries/diary-entries.service';
+import {
+  User as UserModel,
+  DiaryEntries as DiaryEntriesModel,
+} from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly diaryEntriesService: DiaryEntriesService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Get('diary-entries/:id')
+  async getPostById(@Param('id') id: string): Promise<DiaryEntriesModel> {
+    return this.diaryEntriesService.diaryEntry({ id: Number(id) });
+  }
+
+  @Post('diary-entries')
+  async createDiaryEntry(
+    @Body()
+    diaryEntryData: {
+      title: string;
+      content?: string;
+      authorEmail: string;
+    },
+  ): Promise<DiaryEntriesModel> {
+    const { title, content, authorEmail } = diaryEntryData;
+    return this.diaryEntriesService.createDiaryEntries({
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    });
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body()
+    userData: {
+      username: string;
+      email: string;
+      password: string;
+    },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Delete('diary-entries/:id')
+  async deleteDiaryEntry(@Param('id') id: string): Promise<DiaryEntriesModel> {
+    return this.diaryEntriesService.deleteDiaryEntries({ id: Number(id) });
   }
 }
