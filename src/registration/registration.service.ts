764ed16fec30d33206c828cd168ed02f6f6c6f25
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-05-10 16:46:09 +0100

    working on exceptions

--- src/registration/registration.service.ts
+++ src/registration/registration.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { UpdateRegistrationDto } from './dto/update-registration.dto';
@@ -12,6 +12,17 @@ export class RegistrationService {
 
   async registerUser(data: Prisma.UserCreateInput): Promise<any> {
     try {
+      const findUser = await this.prisma.user.findUnique({
+        where: {
+          username: data.username,
+        },
+      });
+      if (findUser) {
+        throw new HttpException(
+          'Username already taken.',
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        );
+      }
       const user = await this.prisma.user.create({ data });
       return user;
     } catch (error) {
