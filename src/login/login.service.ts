4d82143472b5544ebd35639d825aed88c7e8b40d
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-06-13 22:38:57 +0100

    created login fn & jwt token

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateLoginDto } from './dto/create-login.dto';
 import { UpdateLoginDto } from './dto/update-login.dto';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Injectable()
 export class LoginService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(username, pass);
+    if (user?.password !== pass) {
+      throw new HttpException(
+        'Wrong credentials!',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const payload = { sub: user.id, username: user.username };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload, {
+        secret: jwtConstants.secret,
+      }),
+      ...user,
+    };
+    // return user;
+  }
+
   create(createLoginDto: CreateLoginDto) {
     return 'This action adds a new login';
   }
@@ -12,7 +47,7 @@ export class LoginService {
     return `This action returns all login`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} login`;
   }
 
